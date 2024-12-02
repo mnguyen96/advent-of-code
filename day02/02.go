77c7737b7211e8dee8e31ed2f3baa5d05ce539e7
Michael Nguyen <michaelalexnguyen@gmail.com>
2024-12-02 17:16:43 -0500

    day 2

---
+++ day02/02.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	filePath := os.Args[1]
+	readFile, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	scanner := bufio.NewScanner(readFile)
+
+	var list [][]int
+	for scanner.Scan() {
+		line := scanner.Text()
+		numText := strings.Split(line, " ")
+
+		var numbers []int
+		for _, part := range numText {
+			num, err := strconv.Atoi(part)
+			if err != nil {
+				fmt.Println("Error converting to int:", err)
+				continue
+			}
+			numbers = append(numbers, num)
+		}
+		list = append(list, numbers)
+	}
+	var unsafeCount int
+	for _, report := range list {
+		if !isValid(report) {
+			unsafeCount++
+		}
+	}
+	fmt.Println(len(list) - unsafeCount)
+
+	var safeCount int
+	for _, report := range list {
+		for i := 0; i < len(report); i++ {
+			// make slice without current level
+			slice := make([]int, 0, len(report)-1)
+			slice = append(slice, report[:i]...)
+			slice = append(slice, report[i+1:]...)
+			if isValid(slice) {
+				safeCount++
+				break
+			}
+		}
+	}
+	fmt.Println(safeCount)
+}
+
+func isValid(report []int) bool {
+	var lastSign int
+	for i := 1; i < len(report); i++ {
+		level := report[i]
+		currDiff := level - report[i-1]
+		if currDiff == 0 || abs(currDiff) < 1 || abs(currDiff) > 3 || lastSign > 0 && currDiff < 0 || lastSign < 0 && currDiff > 0 {
+			return false
+		}
+		if currDiff < 0 {
+			lastSign = -1
+		} else if currDiff > 0 {
+			lastSign = 1
+		}
+	}
+	return true
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
